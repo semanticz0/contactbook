fdfa6d90c679c9f3ac147cc6ab6f532d6abd4fa4
semanticz0 <semanticz0@gmail.com>
2021-08-12 05:37:25 +0530

    initial commit

---
+++ contact.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+class Contact:
+
+    def __init__(self, name, phone):
+        self.name = name
+        self.phone = phone
+
+        self.con = sqlite3.connect('persistent.db')
+        self.cur = self.con.cursor()
+
+    def __str__(self):
+        return self.name
+
+    def create_user(self):
+    
+        self.cur.execute("INSERT INTO contact (name, phone) VALUES ('{0}','{1}')".format(self.name, self.phone))
+        self.con.commit()
+
+    def change_name(self, new_name):
+        self.name = new_name
+
+    def change_phone(self, new_phone):
+        self.phone = new_phone
+
+    def get_id(self):
+        self.cur.execute('SELECT contact_id FROM contact WHERE phone="{0}"'.format(self.phone))
+        self.con.commit()
+
