91656facd251f697589ec76384e313c136b610b8
semanticz0 <semanticz0@gmail.com>
2021-08-14 03:21:58 +0530

    Add works, tests deleted

--- cli.py
+++ cli.py
@@ -3,10 +3,20 @@ from contactbook import Contact, ContactBook
 book = ContactBook()
 print("Welcome to Contactbook.")
 
-pref = input("What operation would you like to perform? (index, create, view, delete) ")
+while True: 
+    pref = input("What operation would you like to perform? (index, add, view, delete) ")
 
-if pref == 'index':
-    for row in book.index():
-        print(f"{row[0]}. {row[1]}: {row[2]}")
-else:
-    print("There are no other features available at this time. Thank you for your patience")
+    if pref == 'index':
+        for row in book.index():
+            print(f"{row[0]}. {row[1]}: {row[2]}")
+
+    elif pref == "add":
+        name = input("Enter contact name: ")
+        phone = input("Enter phone number: ")
+
+        book.add(name, str(phone))
+        print("Contact added")
+
+    else:
+        print("There are no other features available at this time. Thank you for your patience")
+        exit()
--- contactbook.py
+++ contactbook.py
@@ -9,23 +9,11 @@ class Contact:
         self.con = sqlite3.connect('persistent.db')
         self.cur = self.con.cursor()
 
-    def __str__(self):
-        return self.name
-
-    def create_user(self):
-    
         self.cur.execute("INSERT INTO contact (name, phone) VALUES ('{0}','{1}')".format(self.name, self.phone))
         self.con.commit()
 
-    def change_name(self, new_name):
-        self.name = new_name
-
-    def change_phone(self, new_phone):
-        self.phone = new_phone
-
-    def get_id(self):
-        self.cur.execute('SELECT contact_id FROM contact WHERE phone="{0}"'.format(self.phone))
-        self.con.commit()
+    def __str__(self):
+        return self.name
 
 class ContactBook:
     def __init__(self):
@@ -36,3 +24,5 @@ class ContactBook:
         self.cur.execute('SELECT * FROM contact')
         return self.cur.fetchall()
 
+    def add(self, name, phone):
+        contact = Contact(name, phone)
