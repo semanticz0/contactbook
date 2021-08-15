1c54ce366e69e7405b7096a0ed8305fbb5ca7278
semanticz0 <semanticz0@gmail.com>
2021-08-15 17:43:55 +0530

    Added update to CLI

--- cli.py
+++ cli.py
@@ -1,26 +1,37 @@
 from contactbook import ContactBook
 
-book = ContactBook()
+contacts = ContactBook()
 print("Welcome to Contactbook.")
 
 while True:
     pref = input("What operation would you like to perform? (index, add, remove) ")
 
     if pref == "index":
-        for number, row in enumerate(book.index()):
+        for number, row in enumerate(contacts.index()):
             print(f"{number} - ID{row[0]}. {row[1]}: {row[2]}")
 
     elif pref == "add":
         name = input("Enter contact name: ")
         phone = input("Enter phone number: ")
 
-        book.add(name, str(phone))
+        contacts.add(name, str(phone))
         print("Contact added")
 
     elif pref == "remove":
         id_ = input("Enter ID: ")
-        book.remove(id_)
+        contacts.remove(id_)
         print("Contact deleted")
+
+    elif pref == "update":
+        id_ = input("Enter ID: ")
+        print(contacts.find(id_))
+        
+        name = input("Enter contact name: ")
+        phone = input("Enter phone number: ")
+        
+        contacts.update(id_, name, phone)
+
+        print("Contact updated")
 
     else:
         print(
