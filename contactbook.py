0410c25596783e119b748a0992b63fc77ff63b18
semanticz0 <semanticz0@gmail.com>
2021-08-18 07:43:23 +0530

    Added auto-commit by setting isolation-level to None

--- contactbook.py
+++ contactbook.py
@@ -3,8 +3,13 @@ from exceptions import InputError
 class ContactDatabase:
    
     # :connection param must be a connection-object compliant with the DB API 2.0 protocol
+    # ContactBook sets isolation-level to None, thereby enabling auto-commit. This may
+    # create other issues when under high load, and may open you up to various vulnerabilities.
+    # However, scale is not an issue here.  
+
     def __init__(self, connection):
         self.con = connection
+        self.con.isolation_level = None
         self.cur = self.con.cursor()
 
     def index(self):
@@ -22,20 +27,18 @@ class ContactDatabase:
         self.cur.execute(
             "INSERT INTO contact (name, phone) VALUES (?, ?)", 
             (contact.name, contact.phone))
-        self.con.commit()
 
     def remove(self, id_):
         self.cur.execute("DELETE FROM contact WHERE contact_id=?",
                 (id_,))
-        self.con.commit()
 
     def update(self, id_, contact):
         self.cur.execute(
                 "UPDATE contact SET name=?, phone=? WHERE contact_id=?",
                 (contact.name, contact.phone, id_))
-        self.con.commit()
 
 class Contact:
     def __init__(self, name, phone):
         self.name = name
         self.phone = phone
+
